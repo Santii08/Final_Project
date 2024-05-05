b484fc915d9e874b810b17ea887cffd435ec028d
Santii08 <dan.santiago.rc2005@gmail.com>
2024-05-05 12:15:44 -0500

    1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>,
+  },
+]);
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>hola</h1>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./routes/Login";
+import Sign_up from "./routes/Sign_up";
+import Home from "./routes/Home";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login></Login>,
+  },
+  {
+    path: "/sign",
+    element: <Sign_up></Sign_up>,
+  },
+  {
+    path: "/home",
+    element: <Home></Home>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
